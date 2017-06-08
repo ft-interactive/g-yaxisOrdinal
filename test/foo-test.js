4d278b73588cec80fbacae858e97cb590fd6dfea
Bob Haslett <bob.haslett@ft.com>
2017-06-08 10:52:20 +0100

    Update build

--- test/foo-test.js
+++ test/foo-test.js
@@ -2,6 +2,6 @@ var tape = require("tape"),
     axis = require("../");
 
 tape("foo() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-  test.equal(axis.yaxisOrdinal(), 42);
+  test.equal(axis.yaxisOrdinal().labelWidth(), 0);
   test.end();
 });
